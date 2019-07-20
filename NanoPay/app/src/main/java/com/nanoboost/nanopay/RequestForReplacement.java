586ffcaca5c62248e2bef72d58a3140342f69dcd
kmncornejo <kmncornejo@gmail.com>
2019-07-21 02:57:50 +0800

    wawers

---
+++ NanoPay/app/src/main/java/com/nanoboost/nanopay/RequestForReplacement.java
@@ -0,0 +1,13 @@
+package com.nanoboost.nanopay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestForReplacement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_for_replacement);
+    }
+}
