0b117da6064c67d86fe5c9c667a7a097591d01b6
Benjamin Landicho <roan12695@yahoo.com>
2019-07-20 14:04:28 +0800

    Databasehelper amend

--- NanoPay/app/src/main/java/com/nanoboost/nanopay/DatabaseHelper.java
+++ NanoPay/app/src/main/java/com/nanoboost/nanopay/DatabaseHelper.java
@@ -6,22 +6,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "NanoPay.db";
-    public static final String TABLE_NAME = 'Product Details';
-    public static final String COL_1 = 'Product_Name';
-    public static final String COL_2 = 'Order_No.';
-    public static final String COL_3 = 'Date_of_Purchase';
-    public static final String COL_3 = 'Warranty_Period';
-    public static final String COL_4 = 'Confirmed?';
-    public static final String COL_5 = 'For Return?';
+    public static final String TABLE_NAME = "Product Details";
+    public static final String COL_1 = "Product_Name";
+    public static final String COL_2 = "Order_No.";
+    public static final String COL_3 = "Date_of_Purchase";
+    public static final String COL_4 = "Warranty_Period";
+    public static final String COL_5 = "Confirmed?";
+    public static final String COL_6 = "For Return?";
 
-    public DatabaseHelper(@androidx.annotation.Nullable Context context) {
+    public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
         SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("Create table" + TABLE_NAME + " (Order_No. INTEGER PRIMARY KEY AUTOINCREMENT, Product_Name TEXT," +
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (Order_No. INTEGER PRIMARY KEY AUTOINCREMENT, Product_Name TEXT," +
                 "Date_of_Purchase DATE, Warranty_Period TEXT, Confirmed? TEXT, For_Return? TEXT)");
     }
 
