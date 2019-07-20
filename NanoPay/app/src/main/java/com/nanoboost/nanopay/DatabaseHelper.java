fcc18bc471f4996c21effadb056318e3433244b7
Benjamin Landicho <roan12695@yahoo.com>
2019-07-20 13:32:15 +0800

    Database Insertion Class

---
+++ NanoPay/app/src/main/java/com/nanoboost/nanopay/DatabaseHelper.java
@@ -0,0 +1,33 @@
+package com.nanoboost.nanopay;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "NanoPay.db";
+    public static final String TABLE_NAME = 'Product Details';
+    public static final String COL_1 = 'Product_Name';
+    public static final String COL_2 = 'Order_No.';
+    public static final String COL_3 = 'Date_of_Purchase';
+    public static final String COL_3 = 'Warranty_Period';
+    public static final String COL_4 = 'Confirmed?';
+    public static final String COL_5 = 'For Return?';
+
+    public DatabaseHelper(@androidx.annotation.Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table" + TABLE_NAME + " (Order_No. INTEGER PRIMARY KEY AUTOINCREMENT, Product_Name TEXT," +
+                "Date_of_Purchase DATE, Warranty_Period TEXT, Confirmed? TEXT, For_Return? TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
+    }
+}
